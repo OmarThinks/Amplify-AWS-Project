85eeed31714abbcfce535e132f503a35a3e4836f
Omar Magdy <omar.magdy.code@gmail.com>
2022-02-11 09:06:20 +0200

    Simple edit

--- react-amplified/src/App.js
+++ react-amplified/src/App.js
@@ -80,4 +80,4 @@ const styles = {
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default withAuthenticator(App)
+export default App
